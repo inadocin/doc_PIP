1ea3cd7c7a5041e321d9d748c53b070f11eaf5d6
Nicolas Barrios <ndbarrios@protonmail.com>
2023-02-27 08:52:04 -0300

    Pre viaje

--- data/data.lua
+++ data/data.lua
@@ -1,11 +1,13 @@
+-- para imprimir alguno de los datos de aca, hacer por ejemplo
+-- \lua{tex.print(myData.n)}
 myData = {}
 
 myData.n = 3  -- numero de paletas
 myData.R = 116.1  -- mm
 myData.r = 44.1 -- radio del circ. en el que esta circunscipto la paleta
 myData.rc = 9 -- relacion de compresion
-myData.hc = 44.1 -- altura de camara
-myData.hp = 29.4 -- altura de ranura del puerto_
+myData.hc = 44.1 -- altura de camara en mm
+myData.hp = 29.4 -- altura de ranura del puerto_en mm
 myData.V0 = 500
 myData.Ri = math.sqrt(myData.R^2 - myData.r^2)
 myData.Re = math.sqrt(myData.R^2 + 3*myData.r^2)
